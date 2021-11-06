b2d23b250b99878affdc9ea95785e742bf6cfc1a
iphone <73981769+louxsoen@users.noreply.github.com>
2021-11-07 01:04:21 +0900

    za

--- submit_a/6w_2.cpp
+++ submit_a/6w_2.cpp
@@ -1,32 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class Person{
+class Person {
     int id;
     double weight;
     string name;
 public:
-    Person()
-    {
-        id = 1;
-        weight = 20.5;
-        name = "Grace";
-    }
-    Person(int id, string name)
+    Person(int id = 1, string name = "Grace", double weight = 20.5)
     {
         this->id = id;
-        weight = 20.5;
-        this->name = name;
-    }
-    Person(int id, string name, double weight)
-    {
-        this->id = id;
-        this->name = name;
         this->weight = weight;
+        this->name = name;
     }
     void show() { cout << id << " " << weight << " " << name << endl; }
 };
-
 int main()
 {
     Person grace, ashley(2, "Ashley"), helen(3, "Helen", 32.5);
